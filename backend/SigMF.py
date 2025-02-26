5c1e5e04352fe3211dccaa284dacec53f4d9c2ac
Grace M. Johnson <gmjohnso@hamilton.edu>
2025-02-26 16:53:14 -0500

    classes

--- backend/SigMF.py
+++ backend/SigMF.py
@@ -1,30 +1,43 @@
 import json
-
+import Annotation
 
 class SigMF():
-
+    """
+        Description: Initializes SigMF object
+        :param sigmf_file: the sigmf metadata file to be parsed
+    """
     def __init__(self, sigmf_file):
         sigmf = sigmf_file.read()
         data = json.loads(sigmf)
 
-        try:
-            self.datatype = data['global']['datatype']
-            self.sample_rate = data['global']['core:sample_rate']
-            self.center_frequency = data['captures']['frequency']
-            self.sample_start = data['captures']['sample_start']
+        self.datatype = data['global'].get('core:datatype')
+        self.sample_rate = data['global'].get('core:sample_rate')
+        self.author = data['global'].get('core:author')
+        self.hardware = data['global'].get('core:hw')
+        self.offset = data['global'].get('core:offset')
+        self.recorder = data['global'].get('core:recorder')
 
-        except:
-            self.sample_rate = 
 
-        # self.sample_rate
-        # self.center_frequency
-        # self.
+        self.datetime = data['captures'][0].get('core:datetime')
+        self.center_frequency = data['captures'][0].get('core:frequency')
+        self.sample_start = data['captures'][0].get('core:sample_start')
 
+        self.annotations = []
+
+        for annotation in data['annotations']:
+            sample_start = annotation.get('sample_start')
+            sample_count = annotation.get('sample_count')
+            freq_lower = annotation.get('freq_lower_edge')
+            freq_upper = annotation.get('freq_upper_edge')
+            label = annotation.get('label')
+            comment = annotation.get('comment')
+            Annotation(sample_start, sample_count, freq_lower, freq_upper, label, comment)
 
 def main():
     filename = "qpsk_in_noise.sigmf-meta"
     f = open(filename,)
     sig = SigMF(f)
+    print(sig)
 
 
 if __name__ == "__main__":
